f853a3e0d0c20ef34e37b91b23d3a07dbd3ade84
jihengzhang88 <email2jiheng@gmail.com>
2023-06-14 22:50:28 -0700

    python sample code commit

---
+++ TwoSum.py
@@ -0,0 +1,7 @@
+'''two sum'''
+# n1 = 1
+# n2 = 2
+n1 = float(input('first number:'))    #ctrl+D copy current line, default input is string, need to define it as float or int
+n2 = float(input('second number:'))
+sum = n1 + n2
+print('n1+n2=',sum)
